f03cb8442a3a508735bc549e372435890493165e
3113y <448592981@qq.com>
2025-10-30 22:55:50 +0800

    v0.0.0_2510272251 add magic background

--- scripts/world.lua
+++ scripts/world.lua
@@ -4,3 +4,7 @@ function TBoN_MOD:Spawn_Magic()
     return TBoN.World.Variable.Item.Magic
 end
 TBoN_MOD:AddCallback(ModCallbacks.MC_PRE_GET_COLLECTIBLE, TBoN_MOD.Spawn_Magic)
+function TBoN_MOD:Pickup_Magic_Render(entity)
+    entity:GetSprite():ReplaceSpritesheet(1, "resources/gfx/items/magic_item.png")
+end
+TBoN_MOD:AddCallback(ModCallbacks.MC_POST_PICKUP_RENDER, TBoN_MOD.Pickup_Magic_Render, 100)
