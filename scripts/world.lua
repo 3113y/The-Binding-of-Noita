a1a151dbe925e0833cbefffc8b27278aa3489372
3113y <448592981@qq.com>
2025-11-02 00:51:21 +0800

    v0.0.0_2511020050  add  new pickup

--- scripts/world.lua
+++ scripts/world.lua
@@ -1,10 +1,15 @@
 include("scripts.worlds.world_used_functions")
+
 TBoN.World.Variable.Item.Magic = Isaac.GetItemIdByName("Magic")
+
 function TBoN_MOD:Spawn_Magic()
     return TBoN.World.Variable.Item.Magic
 end
+
 TBoN_MOD:AddCallback(ModCallbacks.MC_PRE_GET_COLLECTIBLE, TBoN_MOD.Spawn_Magic)
+
 function TBoN_MOD:Pickup_Magic_Render(entity)
     entity:GetSprite():ReplaceSpritesheet(1, "resources/gfx/items/magic_item.png")
 end
+
 TBoN_MOD:AddCallback(ModCallbacks.MC_POST_PICKUP_RENDER, TBoN_MOD.Pickup_Magic_Render, 100)
